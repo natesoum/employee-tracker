c5c6522efc39c241beefd6fcf4d7c1ff532bfd45
natesoum <soumnathun@gmail.com>
2022-08-22 21:13:46 -0400

    Updated salary numbers

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,15 +10,15 @@ INSERT INTO department (name)
 VALUES ("Marketing");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Lead", 1000, 1);
+VALUES ("Sales Lead", 100000, 1);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Lead Engineer", 2000, 2);
+VALUES ("Lead Engineer", 200000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Software Engineer", 3000, 2);
+VALUES ("Software Engineer", 90000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Accountant", 4000, 3);
+VALUES ("Accountant", 125000, 3);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Marketing Lead", 5000, 4);
+VALUES ("Marketing Lead", 140000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Tony", "Chopper", 1, 3);
